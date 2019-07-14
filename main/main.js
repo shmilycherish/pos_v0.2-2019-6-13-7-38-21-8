acbc6fbd11f2ddbb78e656493c9ac6d1c0998721
cyang <shmilycherish@gmail.com>
2019-07-14 10:00:01 +0800

    print receipt

--- main/main.js
+++ main/main.js
@@ -1,5 +1,52 @@
 'use strict';
 
+const allItems = loadAllItems();
+
 function printReceipt(inputs) {
-  console.log('请在此实现练习要求，并改写该行代码。');
+  let items = generateItemDetails(inputs);
+  let receipt = generateReceiptTemplate(generateReciptBody(items), getTotalCost(items))
+  console.log(receipt);
+}
+
+function generateItemDetails(inputs) {
+  return inputs.reduce((result, itemBarcode) => {
+    let item = result.filter(itemDetail => itemDetail.itemInfo.barcode == itemBarcode);
+    if(item.length !== 0) {
+      item[0].count++;
+      item[0].total += item[0].itemInfo.price;
+    } else {
+      let itemOriginInfo = findItemDetailByBarcode(itemBarcode);
+      result.push({itemInfo: findItemDetailByBarcode(itemBarcode), count: 1, total:itemOriginInfo.price})
+    }
+    return result;
+  }, []);
+}
+
+function findItemDetailByBarcode(barcode) {
+  let item = allItems.filter(item => item.barcode === barcode)[0];
+  return item;
+}
+
+function generateReceiptTemplate(itemDetails, totalCost) {
+  return `***<没钱赚商店>收据***
+${itemDetails}
+----------------------
+总计：${formatPrice(totalCost)}(元)
+**********************`
+}
+
+function getTotalCost(items) {
+  return items.reduce((totalCost, item) => totalCost + item.total, 0)
+}
+
+function gernerateItemDescription(itemDetail) {
+  return `名称：${itemDetail.itemInfo.name}，数量：${itemDetail.count}${itemDetail.itemInfo.unit}，单价：${formatPrice(itemDetail.itemInfo.price)}(元)，小计：${formatPrice(itemDetail.total)}(元)`;
+}
+
+function generateReciptBody(items) {
+  return items.map(item => gernerateItemDescription(item)).join("\n");
+}
+
+function formatPrice(price) {
+  return price.toFixed(2);
 }
